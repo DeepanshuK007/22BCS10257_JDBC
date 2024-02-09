a3d7a6df77dceae0b878b1e3f182f1c3a6f96c78
DEEPANSHU KUMAR <97106571+DeepanshuK007@users.noreply.github.com>
2024-02-09 12:19:59 +0530

    Update Connection_Class.java

--- Connection_Class.java
+++ Connection_Class.java
@@ -1,5 +1,27 @@
 package com.capgemini.main;
 
-public class Connection_Class {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+//import java.util.Scanner;
 
+public class Connection_Class {
+	public Connection getConnection() throws ClassNotFoundException, SQLException { //getConnection is a user defined func
+		Connection con = null; 
+//		
+	//Here Connection is an iterface and con is an obj but not of this 
+//		interface as it doesnot have an obj thus it is an obj of the class which has this 'Connection' interface, as it has abstract methods in it
+//		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+//		// Here the Class is the interface
+//		
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cuims", "root", "Deepu#007");
+//		System.out.println("Connection Established");
+//		
+		System.out.println(con.getClass().getName());
+//		//Thus con is an object of jdbc class -> ConnectionImpl method's
+		
+		
+		return con;
+	}
 }
